5b0bcf1e1ad4e0d997c4cd85e95f21a396863701
sanjanakumarasingha <sanjanakumarasingha1@gmail.com>
2024-06-18 08:15:48 +0530

    Dashboard basic

--- Server/app.js
+++ Server/app.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const server = () => {
-    console.log('You are listning to port:', PORT);
-}
+    console.log("You are listning to port:", PORT);
+};
 
-server()
+server();
