14e6dd723fc5f3b61ac2a99fa4b22d8aa47336c5
SanjanaKumarasingha <Sanjanakumarasingha1@gmail.com>
2024-07-08 22:28:34 +0530

    Home page head

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,28 +16,7 @@ class MoneyTracker extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Money Tracker'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      home: const Home(),
     );
   }
 }
