cad8ac7c7b1a93bb08e277e7dc4f1a89a38acbeb
SanjanaKumarasingha <sanjanakumarasingha1@gmail.com>
2026-02-16 19:26:39 +0530

    feat: refactor NavbarOverlay to use Link component for navigation

--- Client/src/components/NavbarOverlay.tsx
+++ Client/src/components/NavbarOverlay.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { navItems } from './Navbar';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useMenu } from '../provider/MenuOpenProvider';
 import OpenCloseIcon from './OpenCloseIcon';
 import { useDarkMode } from '../provider/DarkModeProvider';
 
 type Props = {};
 
-const NavbarOverlay = (props: Props) => {
-  const navigate = useNavigate();
+function NavbarOverlay(props: Props) {
+  // const navigate = useNavigate();
   const { isSideBarOpen, toggle } = useMenu();
   const { isDarkMode } = useDarkMode();
 
@@ -22,27 +22,22 @@ const NavbarOverlay = (props: Props) => {
           stroke={4}
         />
       </div>
-
-      <div
-        className="text-2xl py-2 px-4 cursor-pointer w-fit text-primary-300 font-semibold"
-        onClick={() => {
-          navigate('/');
-        }}
-      >
-        <p>Expense</p>
-        <p>Tracker</p>
-      </div>
+      <Link to="/">
+        <div className="text-2xl py-2 px-4 cursor-pointer w-fit text-primary-300 font-semibold hover:scale-103 scale-100 transition-all duration-300">
+          <p>Expense</p>
+          <p>Tracker</p>
+        </div>
+      </Link>
       <div className="pt-4 text-lg flex flex-col gap-2 px-2 text-white">
         {navItems.map((item) => (
-          <a
-            href={item.path}
+          <Link
+            to={item.path}
             key={item.name}
             className="flex gap-2 items-center cursor-pointer hover:bg-primary-200 active:bg-primary-100 rounded-md m-1 p-1 hover:scale-105 scale-100 transition-all duration-300 "
           >
             <span className="text-2xl">{item.icon}</span>
-
             {item.name}
-          </a>
+          </Link>
         ))}
       </div>
     </div>
